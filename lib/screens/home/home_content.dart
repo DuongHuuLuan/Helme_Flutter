e4e149084c40c4b072bcba6eec30864448add8f9
DuongHuuLuan <luanb2205946@student.ctu.edu.vn>
2025-09-06 23:54:11 +0700

    ProductStyleCard

--- lib/screens/home/home_content.dart
+++ lib/screens/home/home_content.dart
@@ -31,27 +31,159 @@ class HomeContent extends StatelessWidget {
         }
 
         final intStyles = groupProduct.keys.toList();
-        return ListView.builder(
-          itemCount: intStyles.length,
-          itemBuilder: (context, index) {
-            final type = intStyles[index];
-            final productOfStyle = groupProduct[type]!;
-            final firstStyleProduct = productOfStyle.first;
-            return SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
-                  ),
-                  ProductCard(product: firstStyleProduct),
-                ],
+
+        return SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: intStyles.length,
+                itemBuilder: (context, index) {
+                  final type = intStyles[index];
+                  final productOfStyle = groupProduct[type]!;
+                  final firstStyleProduct = productOfStyle.first;
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 8,
+                        ),
+                      ),
+                      ProductCard(product: firstStyleProduct),
+                    ],
+                  );
+                },
               ),
-            );
-          },
+              Container(
+                color: Color.fromARGB(255, 0, 0, 0),
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 8,
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    SizedBox(
+                      height: 210,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: intStyles.length,
+                        itemBuilder: (context, index) {
+                          // final product = products[index];
+                          final type = intStyles[index];
+                          final productOfStyle = groupProduct[type]!;
+                          final firstProductStyle = productOfStyle.first;
+                          return Container(
+                            width: 160,
+                            margin: const EdgeInsets.symmetric(horizontal: 16),
+                            child: ProductAllStyleCard(
+                              product: firstProductStyle,
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Container(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ...intStyles.map((style) {
+                      final productOfStyle = groupProduct[style]!;
+                      final title = style.toUpperCase().replaceAll('_', '');
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 8,
+                          vertical: 8,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'MŨ BẢO HIỂM ${title}',
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            SizedBox(
+                              height: 350,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: productOfStyle.length,
+                                itemBuilder: (context, index) {
+                                  return Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 8,
+                                      vertical: 8,
+                                    ),
+                                    child: ProductStyleCard(
+                                      product: productOfStyle[index],
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                      // return Padding(
+                      //   padding: const EdgeInsets.symmetric(vertical: 5),
+                      //   child: Column(
+                      //     crossAxisAlignment: CrossAxisAlignment.center,
+                      //     children: [
+                      //       Text(
+                      //         'MŨ BẢO HIỂM ${title}',
+                      //         style: const TextStyle(
+                      //           fontSize: 22,
+                      //           fontWeight: FontWeight.bold,
+                      //         ),
+                      //       ),
+                      //       const SizedBox(height: 10),
+                      //       SizedBox(
+                      //         height: 350,
+                      //         child: ListView.builder(
+                      //           scrollDirection: Axis.horizontal,
+                      //           itemCount: productOfStyle.length,
+                      //           itemBuilder: (context, index) {
+                      //             return Padding(
+                      //               padding: EdgeInsets.only(
+                      //                 left: 16,
+                      //                 right: index == productOfStyle.length - 1
+                      //                     ? 16
+                      //                     : 0,
+                      //               ),
+                      //               child: ProductStyleCard(
+                      //                 product: productOfStyle[index],
+                      //               ),
+                      //             );
+                      //           },
+                      //         ),
+                      //       ),
+                      //     ],
+                      //   ),
+                      // );
+                    }),
+                  ],
+                ),
+              ),
+            ],
+          ),
         );
       },
     );
--- lib/screens/product/product_card.dart
+++ lib/screens/product/product_card.dart
@@ -1,7 +1,10 @@
+import 'package:app_flutter/screens/cart/cart_screen.dart';
+import 'package:app_flutter/services/cart_services.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:app_flutter/models/product_model.dart';
 import 'package:app_flutter/screens/product/product_detail_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
@@ -38,6 +41,8 @@ class ProductCard extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                       color: Colors.black,
                     ),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
                   ),
                   const SizedBox(height: 20),
                   ElevatedButton(
@@ -74,6 +79,7 @@ class ProductCard extends StatelessWidget {
                 ],
               ),
             ),
+            const SizedBox(width: 10),
             SizedBox(
               width: 150,
               height: 150,
@@ -90,7 +96,7 @@ class ProductCard extends StatelessWidget {
                   ),
                   CachedNetworkImage(
                     imageUrl: product.imageUrl,
-                    width: 150,
+                    width: double.infinity,
                     height: 150,
                     fit: BoxFit.contain,
                     placeholder: (context, url) =>
@@ -98,6 +104,172 @@ class ProductCard extends StatelessWidget {
                     errorWidget: (context, url, error) => Icon(Icons.error),
                   ),
                 ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProductAllStyleCard extends StatelessWidget {
+  final Product product;
+  ProductAllStyleCard({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return ProductDetailScreen(product: product);
+            },
+          ),
+        );
+      },
+      child: Container(
+        width: 150,
+        decoration: BoxDecoration(
+          color: const Color(0xFFF0F0F0),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: SizedBox(
+                height: 150,
+                width: 150,
+                child: CachedNetworkImage(
+                  imageUrl: product.imageUrl,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) {
+                    return const Center(child: CircularProgressIndicator());
+                  },
+                  errorWidget: (context, url, error) {
+                    return Icon(Icons.error);
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            const Divider(color: Color.fromARGB(255, 162, 85, 3)),
+            Text(
+              'MŨ BẢO HIỂM ${product.style}',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                // color: Colors.white,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProductStyleCard extends StatelessWidget {
+  final Product product;
+  final cartServices = CartServices();
+  final userId = FirebaseAuth.instance.currentUser!.uid;
+  ProductStyleCard({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return ProductDetailScreen(product: product);
+            },
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 255, 255, 255),
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Color.fromARGB(255, 202, 201, 201),
+            width: 2,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ClipRRect(
+              child: SizedBox(
+                height: 150,
+                width: 150,
+                child: CachedNetworkImage(
+                  imageUrl: product.imageUrl,
+                  fit: BoxFit.cover,
+                  placeholder: (context, url) {
+                    return const Center(child: CircularProgressIndicator());
+                  },
+                  errorWidget: (context, url, error) {
+                    return Icon(Icons.error);
+                  },
+                ),
+              ),
+            ),
+            // hiển thị size nón
+            Text(
+              product.name,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              '${product.price} đ',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: userId == null
+                  ? null
+                  : () async {
+                      await cartServices.addToCart(userId, product);
+                      await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return CartScreen();
+                          },
+                        ),
+                      );
+                    },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFC7955F),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 16,
+                ),
+              ),
+              child: Text(
+                'Thêm vào giỏ hàng',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
             ),
           ],
